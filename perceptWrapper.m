d4effb1bd6d9e91b825d66e13eb143164a508f1e
Students <student@pharm.otago.ac.nz>
2021-01-22 15:35:50 +1300

    Changes to instructions

--- perceptWrapper.m
+++ perceptWrapper.m
@@ -4,27 +4,30 @@ function perceptWrapper
 
 clear all
 clc
-addpath('mypsychtoolbox')
+% addpath('mypsychtoolbox')
 KbName('UnifyKeyNames');
-PsychJavaTrouble()
+% PsychJavaTrouble()
+
 %% Parameters
+
 p = perceptGetParams;
 Screen('TextSize',p.frame.ptr,24);
+% HideCursor;
 
-HideCursor;
-%% Introduct ion
+%% Introduction
+
 DrawText(p.frame.ptr,{'Welcome to this experiment!',' ',...
     'Press space bar to find out what the task involves!'},'c');
 Screen('Flip', p.frame.ptr);
 WaitSecs(1);
 WaitAnyPress(KbName('space'));
 
-%% Example stimul i
+%% Example stimuli
 
-DrawText(p.frame.ptr,{'You will see two circles on the screen' ,'each with a number of dots inside.',' ' , ...
-    ' ', 'Your task is to try to guess', 'which circle contains the most points.',...
-    ' ', 'Then we will ask you to rate','your confidence in your decision.', ...
-    ' ', 'Please press the space bar to continue'},'c');
+DrawFormattedText(p.frame.ptr,['You will see two circles on the screen\neach with a number of dots inside.\n'...
+    'Your task is to try to guess\nwhich circle contains the most points.\n\n'...
+    'Then we will ask you to rate\nyour confidence in your decision.\n\n', ...
+    'Please press the space bar to continue'],'center', 'center');
 Screen('Flip', p.frame.ptr);
 WaitSecs(2);
 WaitAnyPress(KbName('space'));
@@ -57,10 +60,10 @@ DrawFormattedText(p.frame.ptr,'35 vs 25', 'center', p.my+p.stim.diam+50);
 t=Screen('Flip', p.frame.ptr);
 WaitSecs(3);
 
-DrawText(p.frame.ptr,{'The first part of the task is to choose', 'which circle contains the most points.',...
-    'We will next familiarise you with this part of the task.' , ...
-    'Don''t worry if some of your decisions feel like guesses - it is a hard task!', ...
-    ' ','Press the space bar to continue'},'c');
+DrawFormattedText(p.frame.ptr,['The first part of the task is to choose\nwhich circle contains the most points.\n'...
+    'We will next familiarise you with this part of the task.\n'...
+    'Don''t worry if some of your decisions feel like guesses - it is a hard task!\n\n'...
+    'Press the space bar to continue'],'center','center');
 Screen('Flip', p.frame.ptr);
 WaitSecs(2);
 WaitAnyPress(KbName('space'));
@@ -89,13 +92,11 @@ results = perceptRunBlock(p, feedback, conf, ntrials, staircase_reversal, stepsi
 xc=median(results.contrast(results.i_trial_lastreversal:end)); % contrast at end of block
 
 %% Training on task with confidence rating
-DrawFormattedText(p.frame.ptr, ['We will now give you some practice at using the confidence scale. \n\n After you make a left/right choice,\n' ...
-    'you will see a sliding scale to allow you to rate your confidence in getting the right answer.\n\n'...
+DrawFormattedText(p.frame.ptr, ['We will now give you some practice at using the confidence scale.\n\n After you make a left/right choice,\n' ...
+    'you will see a sliding scale to allow you to\nrate your confidence in getting the right answer.\n\n'...
     'You can move the cursor around on the scale using the left and right arrow keys\n'...
-    'The left end of the scale means that you are less confident than normal, and\n'...
-    'the right end of the scale means that you are more confident than normal.\n\n'...
-    'However, please remember that this is a difficult task - it`s rare that you will be very confident!\n'...
-    'As we are interested in relative changes in confidence, we encourage you to use the whole scale.\n\n' ...
+    'The left end of the scale means that you are not at all confident (guessing).\n'...
+    'The right end of the scale means that you are extremely confident.\n\n'...
     'There won''t be any more feedback as to whether you are right or wrong!\n\n' ...
     '(Press space bar to continue)'], 'center', 'center');
 Screen('Flip', p.frame.ptr);
@@ -104,7 +105,7 @@ WaitAnyPress(KbName('space'));
 
 feedback = 0;
 conf = 1;
-ntrials = 10;
+ntrials = 5; %10;
 staircase_reversal = Inf;
 start_x = xc;
 stepsize = 1;
@@ -112,13 +113,13 @@ adapt = 0;
 results = perceptRunBlock(p, feedback, conf, ntrials, staircase_reversal, stepsize, adapt, start_x);
 
 %% Main task blocks (8 blocks of 25 trials)
-DrawFormattedText(p.frame.ptr, ['We will now ask you to do 8 blocks of 25 trials each, just like in the practice \n\n' ...
+DrawFormattedText(p.frame.ptr, ['We will now ask you to do blocks of 25 trials each, just like in the practice \n\n' ...
     'If you have any questions, please ask the experimenter now! \n\n' ...
     'Otherwise please press the space bar to start...'], 'center', 'center');
 Screen('Flip', p.frame.ptr);
 WaitSecs(0.5);
 WaitAnyPress(KbName('space'));
-nblocks = 8;
+nblocks = 1; %8;
 feedback = 0;
 conf = 1;
 ntrials = 25;
@@ -129,15 +130,17 @@ for b = 1:nblocks
     start_x = xc;
     results = perceptRunBlock(p, feedback, conf, ntrials, staircase_reversal, stepsize, adapt, start_x);
     xc=round(median(results.contrast(results.i_trial_lastreversal:end))); % contrast at end of block
-    DrawFormattedText(p.frame.ptr, ['Take a break! \n\n' ...
-        'Please press the space bar to start the next block when you are ready...'], 'center', 'center');
-    Screen('Flip', p.frame.ptr);
-    WaitSecs(0.5);
-    WaitAnyPress(KbName('space'));    
     DATA(b).results = results;
-    
     save(p.filename,'DATA');
+    if b < nblocks
+        DrawFormattedText(p.frame.ptr, ['Take a break! \n\n' ...
+            'Please press the space bar to start the next block when you are ready...'], 'center', 'center');
+        Screen('Flip', p.frame.ptr);
+        WaitSecs(0.5);
+        WaitAnyPress(KbName('space'));
+    end
 end
 
-%% Save the data and exit
+%% Exit the task
+
 Screen('Closeall')
